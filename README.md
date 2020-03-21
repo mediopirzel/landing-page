6d023e0ba8b4eb4edb3b526e1620a71af2f2100d
Xavier Soler Delgado <xavier@MacBook-MacBook-Pro-de-Xavier.local>
2020-03-21 20:38:47 +0100

    docs: Read me

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Landing Page Project
-
+Front End Developer Nano degree Second project.
 ## Table of Contents
 
-* [Instructions](#instructions)
+### Navigation
+* Navigation is built dynamically as an unordered list in javascript
+* Main navigation only visible when page is on top or scrolling
+* While scrolling through the page, menu shows wich section is active
+* When clicking an item from the navigation menu, the link scrolls to the appropriate section.
+* User can show menu when is hidden going to top or click menu link
 
-## Instructions
+### Sections
+* It's clear which section is being viewed while scrolling through the page.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
-To get started, open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
--- js/app.js
+++ js/app.js
@@ -23,11 +23,12 @@ const parent = document.querySelector('main');
 // all sections
 const sections = parent.querySelectorAll('section');
 
-//seleccionar el ul
+//set variables for building navigation
 const nav = document.querySelector('nav.navbar__menu');
 const navUl = document.getElementById('navbar__list');
 const menuSwitch = nav.querySelector('.switcher__menu');
 
+//scroll top div
 const scrollTop = document.querySelector('.scroll__top')
 
 /**
@@ -36,7 +37,7 @@ const scrollTop = document.querySelector('.scroll__top')
  *
 */
 
-
+// Helper function to know when scrolling ends
 let scrollEnded = function (callback) {
 
 	// Initialize Scrolling variable
@@ -45,13 +46,12 @@ let scrollEnded = function (callback) {
 	// Event
 	window.addEventListener('scroll', function (event) {
 
-		// Clear our timeout throughout the scroll
+		// Clear timeout while scrolling
 		window.clearTimeout(isScrolling);
 
-		// Set a timeout to run after scrolling ends
+		// Set a timeout after scrolling ends
 		isScrolling = setTimeout(function() {
 
-			// Run the callback
 			callback();
 
 		}, 50);
@@ -70,50 +70,65 @@ let scrollEnded = function (callback) {
 // build the nav
 const buildNav = function() {
     sections.forEach(function(item) {
-
+        // Section id for menu link anchors
         let idSection = item.id;
-        let numberSection = idSection.substr(7);
+        // data-nav attribute for menu titles
         let menuLabel = item.getAttribute('data-nav');
 
-        //console.log(item);
+        // add as items as sections
         const htmlTextToAdd =
           `<li>
                 <a href="#${idSection}">${menuLabel}</a>
             </li>`;
         navUl.insertAdjacentHTML('beforeend', htmlTextToAdd);
-        nav.classList.add('show-menu');
+
+
     })
 
+    // By default menu is visible
+    nav.classList.add('show-menu');
 };
 
 buildNav();
 
-// Add class 'active' to section when near top of viewport
+// Add class 'active' to section and menu when near top of viewport
 
 function changeClass(e) {
+    // menu is visible when scrolling
     nav.classList.add('show-menu');
+
     sections.forEach(function(item) {
+
+        // when h2 is visible, the section is active
+        // I think this method is better that when section is on top.
         let title = item.querySelector('h2');
 
+        //with getBoundingClientRect() we can get left, right, top and bottom position
         let positionTitle = title.getBoundingClientRect();
 
         if (
+                // h2 is on screen
                 positionTitle.top >= 0 &&
-                positionTitle.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+                positionTitle.bottom <= (document.documentElement.clientHeight)
             ) {
+                    // Clear previous active sections
                     sections.forEach(function(item) {
                         item.classList.remove('current-section');
                     });
+                    //Set active section
                     item.classList.add('current-section');
                     let itemId = item.id;
-                    //console.log(itemId);
 
+                    let currentMenu = navUl.getElementsByTagName('a');
+                    //Matching current menu and current section using anchor.
+                    let currentLink = navUl.querySelector(`[href='#${itemId}']`);
+
+                    // Clear previous active menus
                     for (let i = 0; i < menuLink.length; i++) {
                         menuLink[i].classList.remove('current-menu');
                     }
-                    let currentMenu = navUl.getElementsByTagName('a');
-                    let currentLink = navUl.querySelector(`[href='#${itemId}']`);
 
+                    //Set active menu
                     currentLink.classList.add('current-menu');
 
                    }
@@ -127,22 +142,27 @@ document.addEventListener('scroll', changeClass);
 
 // Scroll to anchor ID using scrollTO event
 
+//not declared before because is created with javascript
 const menuLink = navUl.querySelectorAll('a');
+//by default first element of navigation is current menu.
 menuLink[0].classList.add('current-menu');
 
 menuLink.forEach(function(item) {
+    //Get section id from anchor
     let idSection = item.getAttribute('href').substring(1);
     let idSectionObject = document.getElementById(idSection);
+    //Get section y position:
     let rectSection = idSectionObject.getBoundingClientRect();
     let sectionY = rectSection.top;
 
+    //Scrolling to section position
     item.addEventListener('click', function(event) {
         window.scrollTo({
           top: sectionY,
           left: 0,
           behavior: 'smooth'
         });
-
+        // Disable anchor
         event.preventDefault();
     })
 
@@ -154,13 +174,15 @@ menuLink.forEach(function(item) {
  *
 */
 
+//Switcher for showing main navigation
 menuSwitch.addEventListener('click', function(item){
     console.log(this.parentNode);
     this.parentNode.classList.add('show-menu');
 });
 
 
-
+// Showing menu if scrolls ends and page is not on top.
+// Hiding 'scroll to top' if scrolls ends and page is on top.
 scrollEnded(function () {
     if(!window.pageYOffset) {
         nav.classList.add('show-menu');
@@ -171,27 +193,11 @@ scrollEnded(function () {
     }
 });
 
+//Scroll top top
 scrollTop.addEventListener('click', function() {
     window.scrollTo({
           top: 0,
-          left: 0,
           behavior: 'smooth'
     });
 });
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
-
-
-
-
-
-
-
-
 

f4f3344df2aa85a8ff9d030f63db723ab31f96a7
Xavier Soler Delgado <xavier@MacBook-MacBook-Pro-de-Xavier.local>
2020-03-21 19:27:35 +0100

    FRONT PAGE, all working

--- js/app.js
+++ js/app.js
@@ -18,6 +18,17 @@
  *
 */
 
+// main parent
+const parent = document.querySelector('main');
+// all sections
+const sections = parent.querySelectorAll('section');
+
+//seleccionar el ul
+const nav = document.querySelector('nav.navbar__menu');
+const navUl = document.getElementById('navbar__list');
+const menuSwitch = nav.querySelector('.switcher__menu');
+
+const scrollTop = document.querySelector('.scroll__top')
 
 /**
  * End Global Variables
@@ -25,6 +36,29 @@
  *
 */
 
+
+let scrollEnded = function (callback) {
+
+	// Initialize Scrolling variable
+	let  isScrolling;
+
+	// Event
+	window.addEventListener('scroll', function (event) {
+
+		// Clear our timeout throughout the scroll
+		window.clearTimeout(isScrolling);
+
+		// Set a timeout to run after scrolling ends
+		isScrolling = setTimeout(function() {
+
+			// Run the callback
+			callback();
+
+		}, 50);
+
+	}, false);
+
+};
 
 
 /**
@@ -34,13 +68,85 @@
 */
 
 // build the nav
+const buildNav = function() {
+    sections.forEach(function(item) {
 
+        let idSection = item.id;
+        let numberSection = idSection.substr(7);
+        let menuLabel = item.getAttribute('data-nav');
+
+        //console.log(item);
+        const htmlTextToAdd =
+          `<li>
+                <a href="#${idSection}">${menuLabel}</a>
+            </li>`;
+        navUl.insertAdjacentHTML('beforeend', htmlTextToAdd);
+        nav.classList.add('show-menu');
+    })
+
+};
+
+buildNav();
 
 // Add class 'active' to section when near top of viewport
+
+function changeClass(e) {
+    nav.classList.add('show-menu');
+    sections.forEach(function(item) {
+        let title = item.querySelector('h2');
+
+        let positionTitle = title.getBoundingClientRect();
+
+        if (
+                positionTitle.top >= 0 &&
+                positionTitle.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+            ) {
+                    sections.forEach(function(item) {
+                        item.classList.remove('current-section');
+                    });
+                    item.classList.add('current-section');
+                    let itemId = item.id;
+                    //console.log(itemId);
+
+                    for (let i = 0; i < menuLink.length; i++) {
+                        menuLink[i].classList.remove('current-menu');
+                    }
+                    let currentMenu = navUl.getElementsByTagName('a');
+                    let currentLink = navUl.querySelector(`[href='#${itemId}']`);
+
+                    currentLink.classList.add('current-menu');
+
+                   }
+            }
+        );
+
+}
+
+document.addEventListener('scroll', changeClass);
 
 
 // Scroll to anchor ID using scrollTO event
 
+const menuLink = navUl.querySelectorAll('a');
+menuLink[0].classList.add('current-menu');
+
+menuLink.forEach(function(item) {
+    let idSection = item.getAttribute('href').substring(1);
+    let idSectionObject = document.getElementById(idSection);
+    let rectSection = idSectionObject.getBoundingClientRect();
+    let sectionY = rectSection.top;
+
+    item.addEventListener('click', function(event) {
+        window.scrollTo({
+          top: sectionY,
+          left: 0,
+          behavior: 'smooth'
+        });
+
+        event.preventDefault();
+    })
+
+});
 
 /**
  * End Main Functions
@@ -48,10 +154,44 @@
  *
 */
 
+menuSwitch.addEventListener('click', function(item){
+    console.log(this.parentNode);
+    this.parentNode.classList.add('show-menu');
+});
+
+
+
+scrollEnded(function () {
+    if(!window.pageYOffset) {
+        nav.classList.add('show-menu');
+        scrollTop.classList.add('hide-top');
+    } else {
+        nav.classList.remove('show-menu');
+        scrollTop.classList.remove('hide-top');
+    }
+});
+
+scrollTop.addEventListener('click', function() {
+    window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+    });
+});
+
 // Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
+
+
+
+
+
+
+
+
+
 
 
